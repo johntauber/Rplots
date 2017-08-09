2cc9f2eb8a87ffeb174f45cdd041cc30948d2c51
johntauber <john.tauber@gmail.com>
2017-08-09 12:22:06 -0400

    Add type conversion for temperature

--- TRPA1_DoubleBarChart.R
+++ TRPA1_DoubleBarChart.R
@@ -6,10 +6,11 @@ library(ggplot2)
 
 colnames(bar2) <- c("temp", "line", "per")
 bar2$line <- factor(bar2$line, levels = unique(bar2$line))
+bar2$temp <- as.character(bar2$temp)
 
 ggplot(bar2, aes(x=line, y=per, fill=temp)) +
   theme(panel.background = element_blank(),
-        axis.text.x = element_text(face = "bold", color = "black", size = 32, angle = 0, hjust=0.5, margin=margin(-15,0,0,0)),
+        axis.text.x = element_text(face = "bold", color = "black", size = 22, angle = 0, hjust=0.5, margin=margin(-15,0,0,0)),
         axis.text.y = element_text(face = "bold", color = "black", size=32, margin=margin(0,10,0,0)),
         axis.title.y = element_text(color = "black", size = 53, vjust=3, margin=margin(0,35,0,0)),
         axis.ticks.x = element_line(color = "white", size=1.5),
@@ -25,4 +26,4 @@ ggplot(bar2, aes(x=line, y=per, fill=temp)) +
   scale_fill_manual(values=c("darkgrey", "black")) 
 
 
-#For 2 groups of 2 export to 1200 x 700 png
+#For 2 groups of 2 export to 1200 x 700 png 
